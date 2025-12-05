705ede5952c7dac72292dfbd378374219c332d74
Nazan <nazan.ozturk37@gmail.com>
2025-12-05 16:06:21 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,4 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Hello Streamlit!")
+st.write("If you see this running, GitHub → Streamlit deployment works.")
