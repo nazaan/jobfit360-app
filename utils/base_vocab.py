a35323e35e655acd8b1a3c20d13d7bfb0e2377bb
Nazan <nazan.ozturk37@gmail.com>
2025-12-06 18:20:33 -0500

    Create base_vocab.py

---
+++ utils/base_vocab.py
@@ -0,0 +1,42 @@
+BASE_VOCAB = {
+    "skills": [
+        # Data / Analytics
+        "Python", "R", "SQL", "Snowflake", "BigQuery", "Redshift",
+        "Databricks", "Spark", "Tableau", "Power BI", "QlikView",
+        "Looker", "Google Data Studio", "Excel", "SPSS", "SAS",
+
+        # Databases
+        "MySQL", "PostgreSQL", "Oracle", "MariaDB", "MongoDB",
+
+        # Cloud
+        "AWS", "Azure", "Google Cloud Platform",
+
+        # Dev / Ops
+        "Git", "GitHub", "GitLab", "Bitbucket", "Docker", "Kubernetes",
+
+        # Marketing / Sales
+        "Google Analytics", "Adobe Analytics", "HubSpot", "Marketo",
+        "Salesforce", "Mailchimp",
+
+        # Design
+        "Photoshop", "Illustrator", "Figma", "Sketch", "InDesign",
+
+        # Product / Collaboration
+        "Jira", "Trello", "Asana", "Monday.com", "ClickUp",
+        "Notion", "Confluence"
+    ],
+
+    "responsibilities": [
+        "led", "managed", "supervised", "mentored", "coordinated",
+        "collaborated", "presented", "reported", "delivered", "designed",
+        "developed", "implemented", "analyzed", "optimized", "negotiated",
+        "trained", "supported", "communicated", "organized", "planned",
+        "executed", "facilitated", "documented", "reviewed", "monitored"
+    ],
+
+    "education": [
+        "Bachelor", "Master", "PhD", "MBA", "BSc", "MSc", "Diploma",
+        "Certification", "PMP", "CPA", "CFA", "High School",
+        "Associate", "Degree"
+    ]
+}
